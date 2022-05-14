c204dbf9926be14c003ed4e9d40ed5e8ac3b62b9
eonnine <jeg910716@gmail.com>
2022-05-14 17:52:28 +0900

    Remove util package

--- src/main/java/com/lims/api/i18n/service/impl/DefaultLocaleMessageSource.java
+++ src/main/java/com/lims/api/i18n/service/impl/DefaultLocaleMessageSource.java
@@ -1,6 +1,6 @@
-package com.lims.api.util.i18n.service.impl;
+package com.lims.api.i18n.service.impl;
 
-import com.lims.api.util.i18n.service.LocaleMessageSource;
+import com.lims.api.i18n.service.LocaleMessageSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
